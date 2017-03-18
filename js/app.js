cc4203c65a54154a43ece6e0cae98b27a30243d2
Gaurav Pandey <gauravv.pandeyy@gmail.com>
2017-03-18 13:07:53 +0530

    Changes to fetch bikes data from json object(Needs beautification)

--- js/app.js
+++ js/app.js
@@ -1,14 +1,34 @@
 var cycleStoreApp = angular.module('CycleStoreApp', ['ngAnimate']);
 cycleStoreApp.controller('CycleStoreController', CycleStoreController);
-function CycleStoreController(SliderImageSvc) {
+function CycleStoreController(SliderImageSvc, SearchDataSvc) {
     this.images = SliderImageSvc.images;
+    var self = this;
+    var promiseBikes = SearchDataSvc.getBikes();
+    promiseBikes.then(function (data) {
+        self.bikes = data;
+    });
     this.showImage = true;
-
     this.toggleShowImage = function () {
-    	this.showImage = false;
+        this.showImage = false;
     }
 
     this.showHomePage = function () {
-    	this.showImage = true;
+        this.enableSearchPage = false;
+        this.enableLoginPage = false;
+        this.showImage = true;
     }
+    this.enableSearchPage = false;
+    this.showSearchPage = function () {
+        this.showImage = false;
+        this.enableLoginPage = false;
+        this.enableSearchPage = true;
+    }
+    this.enableLoginPage = false;
+    this.showLoginPage = function () {
+        this.showImage = false;
+        this.enableSearchPage = false;
+        this.enableLoginPage = true;
+    }
+
+
 }
--- js/appsearch.service.js
+++ js/appsearch.service.js
@@ -1,9 +1,15 @@
 (function () {
     var cycleStoreApp = angular.module("CycleStoreApp");
-    function searchData() {
-        
+    function searchData($http) {
+         var self = this;
+        self.getBikes = function () {
+            var promise1 = $http.get('http://localhost:3000/bikes/');
+            var promise2 = promise1.then(function (response) {
+                return response.data;
+            });
+            return promise2;
+        }
     }
-    
     cycleStoreApp.service("SearchDataSvc", searchData);
 })();
 
