c46136791d16eaa7b86ddf58c38b2418fbd4a182
Anubhav Anand <anubhav.anand@iontrading.com>
2017-03-24 17:53:44 +0530

    Registration module started

--- js/app.js
+++ js/app.js
@@ -3,29 +3,33 @@ var cycleStoreApp = angular.module('CycleStoreApp', ['ngAnimate']);
 cycleStoreApp.controller('CycleStoreController', CycleStoreController);
 
 function CycleStoreController(SliderImageSvc) {
-    this.images = SliderImageSvc.images;
     var self = this;
-    this.bikes = [];
+
+    this.images = SliderImageSvc.images;
+
     this.showImage = true;
     this.enableSearchPage = false;
     this.enableLoginPage = false;
-
-    this.loginName = "";
-    this.searchCategory = "";
-
-    this.toggleShowImage = function () {
-        this.showImage = false;
-    }
+    this.enableRegistrationPage = false;
 
     this.showHomePage = function () {
         this.enableSearchPage = false;
         this.enableLoginPage = false;
         this.showImage = true;
+        this.enableRegistrationPage = false;
     }
 
     this.showLoginPage = function () {
         this.showImage = false;
         this.enableSearchPage = false;
         this.enableLoginPage = true;
+        this.enableRegistrationPage = false;
+    }
+
+    this.showRegistrationPage = function () {
+        this.showImage = false;
+        this.enableSearchPage = false;
+        this.enableLoginPage = false;
+        this.enableRegistrationPage = true;
     }
 }
---
+++ js/register.service.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+	    angular
+	        .module('CycleStoreApp')
+	        .controller('RegisterController', RegisterController);
+
+	    function RegisterController() {
+	    	var self = this;
+
+	    	function register() {
+
+	    	};
+	    }
+
+	}
+
+)();
